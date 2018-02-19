8fb0e7bbef9fff1316a1ab5a18a7d913587c7229
Tobias Ilsøe Jensen <ilsoe.tobias@live.dk>
2018-02-19 16:20:33 +0100

    solution Exercise 3 guesser

--- guesser/Program.cs
+++ guesser/Program.cs
@@ -10,11 +10,43 @@ namespace guesser
     {
         // the "const" keyword makes the value of the variable final, meaning it can never be modified
         // a variable with "const" acts also as if it had the "static" modifier, meaning it can be accessed without needing to instantiate an object
-        const int MaxTrials = 10;
+        const int MaxTrials = 3;
 
         static void Main(string[] args)
         {
             /* TO DO: here is where you add your code! */
+            int i = 0;
+            string password;
+            do
+            {
+                Console.WriteLine("Write your password here");
+                password = Console.ReadLine();
+
+                if (password == "kakao")
+                {
+                    Console.WriteLine("succes");
+                    break;
+                }
+                else
+                {
+                    i++;
+                    
+                    if (i == MaxTrials)
+                    {
+                        Console.WriteLine("Max attempts tried. You cannot enter");
+                        Environment.Exit(0);
+                    }
+                    Console.WriteLine("Password failed, try again");
+                }
+
+
+            } while (i < MaxTrials);
+            {
+              
+            }
+            Console.ReadLine();
+            //not exactly usefull, just to make it easier to read
+        
         }
     }
 }
